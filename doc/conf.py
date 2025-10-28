71242cf30a8c1157e4e8cd567e04d9025466b1e0
SporeVoid <contact@sporevoid.com>
2025-10-28 14:37:06 +0100

    Do not collapse navigation

--- doc/conf.py
+++ doc/conf.py
@@ -26,6 +26,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_theme_options = {
+    'collapse_navigation': False,
+}
 html_context = {
   'display_github': True,
   'github_user': 'Spore-Void',
